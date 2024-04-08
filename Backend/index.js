c5dc164b13aad6651b379aa2316941c26f3d5adb
nitin562 <nitindbas8800@gmail.com>
2024-04-08 07:17:12 +0530

    More comments

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,8 @@
 const express=require("express")
 const cors=require("cors")
 const app=express()
-const port=8000||process.env.PORT // if env file is present
+const port=8000||process.env.PORT // if env file is present and make sure install dotenv but if you have new version of nodejs then dotenv functionality is built in
+console.log(port)
 app.use(cors()) //cors issue solved
 app.use(express.json()) //json body parser middleware
 //using api modules
